85bbd07320f3eda60021907a6d18bb4ba8d9a885
Osama Najjar <osama.najjar.m@gmail.com>
2023-02-27 23:18:41 +0100

    feat: enhance create new project

--- src/components/projects/ProjectListItem.tsx
+++ src/components/projects/ProjectListItem.tsx
@@ -58,6 +58,7 @@ export function NewProjectListItem({ onClick }: NewProjectListItemProps) {
       <div className='flex h-full w-full flex-col items-center justify-center text-gray-400  hover:text-blue-600'>
         <NewFolder />
         <div className='h5'>New project</div>
+        <p className='mt-1 text-xs'>Create a new a project</p>
       </div>
     </li>
   );
