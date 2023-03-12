61a8c8cd4a426f151ca02a91d8d49a572d0c4351
Osama Najjar <osama.najjar.m@gmail.com>
2023-03-12 06:56:40 +0100

    refactor: cleanup

--- src/desginer/components/Text.tsx
+++ src/desginer/components/Text.tsx
@@ -128,171 +128,94 @@ const TextComponentSettings = () => {
   };
 
   return (
-    <>
-      {/* <Tabs value='font'>
-        <TabsHeader className='w-full rounded-none'>
-          <Tab value='font'>Font</Tab>
-          <Tab value='alignment'>Alignment</Tab>
-        </TabsHeader>
-        <TabsBody>
-          <TabPanel value='font' className='w-full'>
-            <div className='mb-2'>
-              <Typography
-                variant='small'
-                color='blue-gray'
-                className='mb-2 flex flex-row items-center justify-between font-medium'
-              >
-                <span>Font size </span>
-                <span className='w-14 rounded-md bg-indigo-700 py-1 px-2 text-center text-white'>
-                  {style?.fontSize}
-                </span>
-              </Typography>
-              <Slider
-                value={Number(style?.fontSize?.toString().replace('px', ''))}
-                min={8}
-                step={1}
-                max={250}
-                onChange={(_, value) => {
-                  handleFontSizeChanged(value);
-                }}
+    <PanelGroup accordion>
+      <Panel header='Font' defaultExpanded>
+        <span>Size</span>
+        <Stack
+          direction='row'
+          spacing={20}
+          alignItems='center'
+          justifyContent='center'
+        >
+          <Stack.Item grow={1}>
+            <Slider
+              progress
+              min={6}
+              max={100}
+              value={Number(style?.fontSize?.toString().replace('px', ''))}
+              onChange={(value) => {
+                handleFontSizeChanged(Number(value));
+              }}
+            />
+          </Stack.Item>
+          <Stack.Item basis={70}>
+            <InputNumber
+              min={6}
+              max={100}
+              value={Number(style?.fontSize?.toString().replace('px', ''))}
+              onChange={(value) => {
+                handleFontSizeChanged(Number(value));
+              }}
+            />
+          </Stack.Item>
+        </Stack>
+      </Panel>
+      <Panel header='Colors' defaultExpanded>
+        <Stack justifyContent='space-between' alignItems='center'>
+          <Stack.Item>
+            <span>Text color</span>
+          </Stack.Item>
+          <Stack.Item>
+            <BsCircleFill
+              className='pointer-cursor'
+              onClick={toggleTextColor}
+              style={{
+                ...colorIconStyle,
+                color: colorToCSSrgba(ensure(textColor)),
+              }}
+            />
+          </Stack.Item>
+        </Stack>
+        <Stack direction='column' alignItems='flex-end'>
+          {showTextColor ? (
+            <div className='flex justify-center'>
+              <ChromePicker
+                color={textColor?.rgb}
+                onChange={handleTextColorChange}
               />
             </div>
-            <div className='mb-2'>
-              <Typography
-                variant='small'
-                color='blue-gray'
-                className='mb-4 flex flex-row items-center justify-between font-medium'
-              >
-                <span>Text color</span>
-                <span
-                  className='block h-6 w-6 cursor-pointer rounded-full border-2 border-blue-gray-500 shadow-lg'
-                  style={{ background: colorToCSSrgba(ensure(textColor)) }}
-                  onClick={toggleTextColor}
-                ></span>
-              </Typography>
-              {showTextColor ? (
-                <div className='flex justify-center'>
-                  <ChromePicker
-                    color={textColor?.rgb}
-                    onChange={handleTextColorChange}
-                  />
-                </div>
-              ) : null}
+          ) : null}
+        </Stack>
+        <Stack justifyContent='space-between' alignItems='center'>
+          <Stack.Item>
+            <span>Background color</span>
+          </Stack.Item>
+          <Stack.Item>
+            <BsCircleFill
+              className='pointer-cursor'
+              onClick={toggleBgColor}
+              style={{
+                ...colorIconStyle,
+                color: colorToCSSrgba(ensure(bgColor)),
+              }}
+            />
+          </Stack.Item>
+        </Stack>
+        <Stack direction='column' alignItems='flex-end'>
+          {showBgColor ? (
+            <div className='flex justify-center'>
+              <ChromePicker
+                color={bgColor?.rgb}
+                onChange={handleBGColorChange}
+              />
             </div>
-            <div className='mb-2'>
-              <Typography
-                variant='small'
-                color='blue-gray'
-                className='mb-2 flex flex-row items-center justify-between font-medium'
-              >
-                <span>Background color</span>
-                <span
-                  className='block h-6 w-6 cursor-pointer rounded-full border-2 border-blue-gray-500 shadow-lg'
-                  style={{ background: colorToCSSrgba(ensure(bgColor)) }}
-                  onClick={toggleBgColor}
-                ></span>
-              </Typography>
-              {showBgColor ? (
-                <div className='flex justify-center'>
-                  <ChromePicker
-                    color={bgColor?.rgb}
-                    onChange={handleBGColorChange}
-                  />
-                </div>
-              ) : null}
-            </div>
-          </TabPanel>
-        </TabsBody>
-      </Tabs> */}
-      <PanelGroup accordion>
-        <Panel header='Font' defaultExpanded>
-          <span>Size</span>
-          <Stack
-            direction='row'
-            spacing={20}
-            alignItems='center'
-            justifyContent='center'
-          >
-            <Stack.Item grow={1}>
-              <Slider
-                progress
-                min={6}
-                max={100}
-                value={Number(style?.fontSize?.toString().replace('px', ''))}
-                onChange={(value) => {
-                  handleFontSizeChanged(Number(value));
-                }}
-              />
-            </Stack.Item>
-            <Stack.Item basis={70}>
-              <InputNumber
-                min={6}
-                max={100}
-                value={Number(style?.fontSize?.toString().replace('px', ''))}
-                onChange={(value) => {
-                  handleFontSizeChanged(Number(value));
-                }}
-              />
-            </Stack.Item>
-          </Stack>
-        </Panel>
-        <Panel header='Colors' defaultExpanded>
-          <Stack justifyContent='space-between' alignItems='center'>
-            <Stack.Item>
-              <span>Text color</span>
-            </Stack.Item>
-            <Stack.Item>
-              <BsCircleFill
-                className='pointer-cursor'
-                onClick={toggleTextColor}
-                style={{
-                  ...colorIconStyle,
-                  color: colorToCSSrgba(ensure(textColor)),
-                }}
-              />
-            </Stack.Item>
-          </Stack>
-          <Stack direction='column' alignItems='flex-end'>
-            {showTextColor ? (
-              <div className='flex justify-center'>
-                <ChromePicker
-                  color={textColor?.rgb}
-                  onChange={handleTextColorChange}
-                />
-              </div>
-            ) : null}
-          </Stack>
-          <Stack justifyContent='space-between' alignItems='center'>
-            <Stack.Item>
-              <span>Background color</span>
-            </Stack.Item>
-            <Stack.Item>
-              <BsCircleFill
-                className='pointer-cursor'
-                onClick={toggleBgColor}
-                style={{
-                  ...colorIconStyle,
-                  color: colorToCSSrgba(ensure(bgColor)),
-                }}
-              />
-            </Stack.Item>
-          </Stack>
-          <Stack direction='column' alignItems='flex-end'>
-            {showBgColor ? (
-              <div className='flex justify-center'>
-                <ChromePicker
-                  color={bgColor?.rgb}
-                  onChange={handleBGColorChange}
-                />
-              </div>
-            ) : null}
-          </Stack>
-        </Panel>
-        <Panel header='Alignment' defaultExpanded>
-          <Placeholder.Paragraph />
-        </Panel>
-      </PanelGroup>
-    </>
+          ) : null}
+        </Stack>
+      </Panel>
+      <Panel header='Alignment' defaultExpanded>
+        <Placeholder.Paragraph />
+      </Panel>
+    </PanelGroup>
   );
 };
 
