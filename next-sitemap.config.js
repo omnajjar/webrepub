981be07f9956e46473af72fea79f0797070c55c4
Osama Najjar <osama.najjar.m@gmail.com>
2023-03-24 22:51:11 +0100

    chore: configure next sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,7 @@
 module.exports = {
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: process.env.SITE_URL || 'https://webrepub.com',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
