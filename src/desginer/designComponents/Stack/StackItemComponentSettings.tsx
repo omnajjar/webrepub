c54cc90fdadba419336af3d696e85a4c7613ef35
Osama Najjar <osama.najjar.m@gmail.com>
2023-03-26 03:27:07 +0200

    feat: convert stakc item to styled components

--- src/desginer/designComponents/Stack/StackItemComponentSettings.tsx
+++ src/desginer/designComponents/Stack/StackItemComponentSettings.tsx
@@ -1,22 +1,37 @@
 import { useNode } from '@craftjs/core';
-import { StackItemProps } from 'rsuite/esm/Stack/StackItem';
 
+import { ColorStyleControls } from '@/desginer/designComponents/Common/ColorStyleControls';
 import { ComponentPropsControlsContainer } from '@/desginer/designComponents/Common/ComponentPropsControlsContainer';
 import { FlexboxStyleControls } from '@/desginer/designComponents/Common/FlexboxStyleControls';
 import { PaddingMarginStyleControls } from '@/desginer/designComponents/Common/PaddingMarginStyleControls';
+import { StackItemComponentProps } from '@/desginer/designComponents/Stack/StackItemComponent';
 
 export const StackItemComponentSettings = () => {
-  const { style, name } = useNode<Partial<StackItemProps> & { name: string }>(
-    (node) => ({
-      style: node.data.props.style,
-      name: node.data.displayName,
-    })
-  );
+  const { cssProps, name } = useNode<
+    Partial<StackItemComponentProps> & { name: string }
+  >((node) => ({
+    cssProps: node.data.props.cssProps,
+    name: node.data.displayName,
+  }));
 
   return (
     <ComponentPropsControlsContainer componentName={name}>
-      <FlexboxStyleControls style={style} defaultExpanded={true} />
-      <PaddingMarginStyleControls style={style} defaultExpanded={true} />
+      <FlexboxStyleControls
+        style={cssProps}
+        defaultExpanded={true}
+        useStyledComponents
+      />
+      <ColorStyleControls
+        style={cssProps}
+        defaultExpanded={true}
+        allowControls={['bg']}
+        useStyledComponents
+      />
+      <PaddingMarginStyleControls
+        style={cssProps}
+        defaultExpanded={true}
+        useStyledComponents
+      />
     </ComponentPropsControlsContainer>
   );
 };
