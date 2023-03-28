8007541dc49b1e11105a7601738d3184498f9da3
Osama Najjar <osama.najjar.m@gmail.com>
2023-03-29 00:16:18 +0200

    feat: add support to images
    Todo: we should find a future proof scalable apparoch to handle dimesnions and units

--- src/desginer/utils/units.ts
+++ src/desginer/utils/units.ts
@@ -1,25 +1,30 @@
-import { UnsupportedCSStyleUnit } from '@/desginer/utils/errors';
-
 export type CSSValueUnit =
   | { name: 'pixel'; shorthand: 'px'; value: number }
-  | { name: 'precentage'; shorthand: '%'; value: number };
+  | { name: 'precentage'; shorthand: '%'; value: number }
+  | { name: 'auto'; shorthand: 'auto'; value: 'auto' };
 
-export function getValueUnit(value: string): CSSValueUnit {
-  if (value.indexOf('%') !== -1) {
+export function getValueUnit(value: string | number | undefined): CSSValueUnit {
+  const v = value?.toString();
+
+  if (v && v.indexOf('%') !== -1) {
     return {
       name: 'precentage',
       shorthand: '%',
-      value: Number(value.replace('%', '')),
+      value: Number(v.replace('%', '')),
     };
   }
 
-  if (value.indexOf('px') !== -1) {
+  if (v && v.indexOf('px') !== -1) {
     return {
       name: 'pixel',
       shorthand: 'px',
-      value: Number(value.replace('px', '')),
+      value: Number(v.replace('px', '')),
     };
   }
 
-  throw new UnsupportedCSStyleUnit();
+  return {
+    name: 'auto',
+    shorthand: 'auto',
+    value: 'auto',
+  };
 }
