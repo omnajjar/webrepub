a460d7dcff5dc3769904e58d37b1ea0ba2cce6e2
Osama Najjar <osama.najjar.m@gmail.com>
2023-03-29 22:58:45 +0200

    feat: set intial width for design view

--- src/desginer/DesignViewport.tsx
+++ src/desginer/DesignViewport.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Container } from 'rsuite';
 
 import { MainFrame } from '@/desginer/frames/MainFrame';
 
 export function DesignViewport() {
   const [initialHeigh, setIntialHeight] = useState('100%');
+  const [initialWidth, setWidth] = useState('');
+
+  const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (!window) {
@@ -13,12 +16,23 @@ export function DesignViewport() {
 
     // we need to set a fixed height to the design view port so that it can be scrollable.
     setIntialHeight(`${window.innerHeight - 56}px`); // 56px is the header height
+
+    if (ref && ref.current) {
+      const rect = ref.current.getBoundingClientRect();
+
+      setWidth(`${rect.width}px`);
+    }
   }, []);
 
   return (
     <Container
+      ref={ref}
       className='design-view-port flex flex-col items-center'
-      style={{ height: `${initialHeigh}`, padding: '30px' }}
+      style={{
+        height: `${initialHeigh}`,
+        ...(initialWidth ? { width: initialWidth } : {}),
+        padding: '30px',
+      }}
     >
       <MainFrame></MainFrame>
     </Container>
