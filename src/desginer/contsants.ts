f8d3f219e6782e7bc7fb1c128120058e5bb3078f
Osama Najjar <osama.najjar.m@gmail.com>
2023-03-30 02:39:17 +0200

    feat: enable auto-save

--- src/desginer/contsants.ts
+++ src/desginer/contsants.ts
@@ -1,4 +1,5 @@
-export const WEBREPUB_LOCALSTORAGE_KEY = '__WebRepub__JSON';
+export const WEBREPUB_LOCALSTORAGE_KEY_CURRENT_DESIGN =
+  '__WebRepub_JSON_Current_Design';
 
 export const BLANK_DESIGN_JSON_STRING =
   '{"ROOT":{"type":{"resolvedName":"WebPageComponent"},"isCanvas":true,"props":{},"displayName":"Web page","custom":{},"hidden":false,"nodes":[],"linkedNodes":{}}}';
--- src/desginer/frames/PreviewFrame.tsx
+++ src/desginer/frames/PreviewFrame.tsx
@@ -2,13 +2,13 @@ import { Frame } from '@craftjs/core';
 
 import {
   BLANK_DESIGN_JSON_STRING,
-  WEBREPUB_LOCALSTORAGE_KEY,
+  WEBREPUB_LOCALSTORAGE_KEY_CURRENT_DESIGN,
 } from '@/desginer/contsants';
 import { useLocalStorage } from '@/desginer/hooks/useLocalStorage';
 
 export function PreviewFrame() {
   const [designJson] = useLocalStorage(
-    WEBREPUB_LOCALSTORAGE_KEY,
+    WEBREPUB_LOCALSTORAGE_KEY_CURRENT_DESIGN,
     BLANK_DESIGN_JSON_STRING
   );
 
