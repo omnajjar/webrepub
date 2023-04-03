dd62b40c3b6188892f8340aae44a949e9f3ac4ea
Osama Najjar <osama.najjar.m@gmail.com>
2023-04-03 03:03:34 +0200

    feat: add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,28 +1,34 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const socialImageURL = `${process.env.NEXT_PUBLIC_HOST}/images/webrepub-social.png`;
   return (
     <Html lang='en'>
       <title>WebRepub</title>
       <Head>
         <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
-        <meta
-          name='title'
-          content='Open source web page editor using React and  Styled Components 🚀'
-        />
+        <meta name='title' content='WebRepub' />
         <meta
           name='description'
           content='Webrepub is an open source web page editor 🚀 powered🔋 by React ✨ and Styled Components 💅. Our mission is to create a powerful React page editor that supports different presets of modern and popular components and design libraries that will make web development much faster and more fun.'
         />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://webrepub.com/' />
+        <meta property='og:title' content='WebRepub' />
         <meta
-          name='keywords'
-          content='webrepub,react,reactjs,styledcomponent,styled-component,page-editor, website-builder,website-generator,design'
+          property='og:description'
+          content='Webrepub is an open source web page editor 🚀 powered🔋 by React ✨ and Styled Components 💅. Our mission is to create a powerful React page editor that supports different presets of modern and popular components and design libraries that will make web development much faster and more fun.'
         />
-        <meta name='robots' content='index, follow' />
-        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-        <meta name='language' content='English' />
-        <meta name='revisit-after' content='1 days' />
-        <meta name='author' content='Osama Mohammad Najjar' />
+        <meta property='og:image' content={socialImageURL} />
+
+        <meta property='twitter:card' content={socialImageURL} />
+        <meta property='twitter:url' content='https://webrepub.com/' />
+        <meta property='twitter:title' content='WebRepub' />
+        <meta
+          property='twitter:description'
+          content='Webrepub is an open source web page editor 🚀 powered🔋 by React ✨ and Styled Components 💅. Our mission is to create a powerful React page editor that supports different presets of modern and popular components and design libraries that will make web development much faster and more fun.'
+        />
+        <meta property='twitter:image' content={socialImageURL} />
       </Head>
       <body>
         <Main />
