4ddbde2df6ec4c80caf385a9d1873e46e96999fb
Osama Najjar <osama.najjar.m@gmail.com>
2023-04-03 02:34:25 +0200

    feat: add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,23 @@ export default function Document() {
       <title>WebRepub</title>
       <Head>
         <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
+        <meta
+          name='title'
+          content='Open source web page editor using React and  Styled Components 🚀'
+        />
+        <meta
+          name='description'
+          content='Webrepub is an open source web page editor 🚀 powered🔋 by React ✨ and Styled Components 💅. Our mission is to create a powerful React page editor that supports different presets of modern and popular components and design libraries that will make web development much faster and more fun.'
+        />
+        <meta
+          name='keywords'
+          content='webrepub,react,reactjs,styledcomponent,styled-component,page-editor, website-builder,website-generator,design'
+        />
+        <meta name='robots' content='index, follow' />
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta name='language' content='English' />
+        <meta name='revisit-after' content='1 days' />
+        <meta name='author' content='Osama Mohammad Najjar' />
       </Head>
       <body>
         <Main />
